699d184ca7dc001e6535306b775075ef55cdbee7
Piotr Pelc <piotrpelc83@gmail.com>
2024-08-27 19:50:34 +0200

    Assign a value to the playerMove variable in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let playerMove = 'papier';
+let playerMove = 'nieznany ruch';
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
@@ -9,9 +9,23 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 }   else if(randomNumber == 2){
-    computerMove = 'papier';
+        computerMove = 'papier';
 }   else if(randomNumber == 3){
-    computerMove = 'nożyce';
+        computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}   else if(playerInput == '2'){
+        playerMove = 'papier';
+}   else if(playerInput == 3){
+        playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
